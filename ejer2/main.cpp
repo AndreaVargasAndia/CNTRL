cb9607dfc62a39088fb4b9ebc86e56acacb5e30a
Your Name <you@example.com>
2017-09-18 22:40:49 -0500

    Ejercicio

---
+++ ejer2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int lar=7;
+    int arr[]={2,3,4,5,5,7,8},x,inv[]={},i,j;
+    for(i=0;i<lar;i++)
+    {
+        arr[i]=inv[i];
+    }
+    return (j=lar-1;j>=0;j--);
+        {
+            cout<< inv[j] ;
+    }
+
+
+    return 0;
+}
