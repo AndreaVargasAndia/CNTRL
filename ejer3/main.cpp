eab28f5f26f88884cee5d396521833dfe1ddf3b8
Your Name <you@example.com>
2017-09-19 01:29:03 -0500

    Ejercicio

---
+++ ejer3/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int invert(int arr[],int inv[],int lar){
+  for(int i=0;i<lar;i++)
+    {
+      arr[i]=arr[i];
+    }
+    for(int j=lar-1;j>=0; j--){
+        cout<<arr[j];
+      }
+
+
+
+}
+int main()
+{
+    int lar=7;
+    int arr[]={2,3,4,5,5,7,8},x,inv[]={},i,j;
+    cout<<(arr,inv,7);
+    return 0;
+
+
+}
